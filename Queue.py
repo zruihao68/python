e962e0b99e7295315938de114ad4a250f8aabe2b
zrui <zruihao68@163.com>
2016-03-21 15:26:26 +0800

    队列

--- Queue.py
+++ Queue.py
@@ -1,26 +1,31 @@
 #队列的实现
 class Queue(object):
+    #初始化
     def __init__(qu,size):
         qu.queue = []
         qu.size = size
         qu.head = -1
         qu.tail = -1
+    #判断是否为空
     def Empty(qu):
         if qu.head == qu.tail:
             return True
         else:
             return False
+    #判卷队列是否已满
     def Full(qu):
         if qu.tail - qu.head + 1 == qu.size:
             return True
         else:
             return False
+    #进
     def enQueue(qu,content):
         if qu.Full():
             print("Queue is Full!")
         else:
             qu.queue.append(content)
             qu.tail = qu.tail+1
+    #出
     def outQueue(qu):
         if qu.Empty():
             print("Queue is Empty!")
